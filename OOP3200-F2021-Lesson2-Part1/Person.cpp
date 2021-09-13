e9961a8e263e759ca327da879d9a85373842bcba
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2021-09-13 09:38:36 -0400

    added method headers for Person Class

--- OOP3200-F2021-Lesson2-Part1/Person.cpp
+++ OOP3200-F2021-Lesson2-Part1/Person.cpp
@@ -16,28 +16,45 @@ Person::Person(std::string name, int age)// option 1: : m_name(name), m_age(age)
 Person::~Person()
 = default;
 
+/// <summary>
+/// This method returns the value of the m_name member variable
+/// </summary>
 std::string Person::GetName()
 {
 	return m_name;
 }
 
+/// <summary>
+/// This method returns the value of the m_age member variable
+/// </summary>
 int Person::GetAge()
 {
 	return m_age;
 }
 
+/// <summary>
+/// This method takes one string parameter, name, and sets the m_name variable to its value
+/// </summary>
+/// <param name="name"></param>
 void Person::SetName(std::string name = "not set")
 {
 	m_name = name;
 	std::cout << "Name set to: " << name << std::endl;
 }
 
+/// <summary>
+/// This method takes one integer parameter, age, and sets the m_age variable to its value
+/// </summary>
+/// <param name="age"></param>
 void Person::SetAge(int age = 0)
 {
 	m_age = age;
 	std::cout << "Age set to: " << age << std::endl;
 }
 
+/// <summary>
+/// This method outputs the m_name value and says hello
+/// </summary>
 void Person::SaysHello()
 {
 	std::cout << GetName() << " says hello!" << std::endl;
